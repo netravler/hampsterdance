65483048b94772f852989db1380c7be28f666f7e
netravler <pheintz@hotmail.com>
2018-09-13 13:22:52 -0500

    Add files via upload

---
+++ hampsterdance/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace hampsterdance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.excersizeTheHampsterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.getOnTheWheelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyOutTheResultsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadURLsToTestToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.excersizeTheHampsterToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1107, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // excersizeTheHampsterToolStripMenuItem
+            // 
+            this.excersizeTheHampsterToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.getOnTheWheelToolStripMenuItem,
+            this.copyOutTheResultsToolStripMenuItem,
+            this.loadURLsToTestToolStripMenuItem});
+            this.excersizeTheHampsterToolStripMenuItem.Name = "excersizeTheHampsterToolStripMenuItem";
+            this.excersizeTheHampsterToolStripMenuItem.Size = new System.Drawing.Size(166, 24);
+            this.excersizeTheHampsterToolStripMenuItem.Text = "Excercise the Hamster";
+            // 
+            // getOnTheWheelToolStripMenuItem
+            // 
+            this.getOnTheWheelToolStripMenuItem.Name = "getOnTheWheelToolStripMenuItem";
+            this.getOnTheWheelToolStripMenuItem.Size = new System.Drawing.Size(209, 24);
+            this.getOnTheWheelToolStripMenuItem.Text = "Get on the Wheel";
+            this.getOnTheWheelToolStripMenuItem.Click += new System.EventHandler(this.getOnTheWheelToolStripMenuItem_Click);
+            // 
+            // copyOutTheResultsToolStripMenuItem
+            // 
+            this.copyOutTheResultsToolStripMenuItem.Name = "copyOutTheResultsToolStripMenuItem";
+            this.copyOutTheResultsToolStripMenuItem.Size = new System.Drawing.Size(209, 24);
+            this.copyOutTheResultsToolStripMenuItem.Text = "Copy out the results";
+            this.copyOutTheResultsToolStripMenuItem.Click += new System.EventHandler(this.copyOutTheResultsToolStripMenuItem_Click);
+            // 
+            // loadURLsToTestToolStripMenuItem
+            // 
+            this.loadURLsToTestToolStripMenuItem.Name = "loadURLsToTestToolStripMenuItem";
+            this.loadURLsToTestToolStripMenuItem.Size = new System.Drawing.Size(209, 24);
+            this.loadURLsToTestToolStripMenuItem.Text = "Load URL\'s to test";
+            this.loadURLsToTestToolStripMenuItem.Click += new System.EventHandler(this.loadURLsToTestToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(557, 54);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(538, 284);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 54);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(538, 284);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(808, 0);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(148, 22);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(808, 31);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(148, 22);
+            this.textBox4.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(751, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Time In";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(740, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Time Out";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "*.txt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1107, 358);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Hamster Dance";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem excersizeTheHampsterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem getOnTheWheelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyOutTheResultsToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem loadURLsToTestToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ hampsterdance/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//
+using System.Net;
+using System.Net.NetworkInformation;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace hampsterdance
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void getOnTheWheelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+
+            //string[] testURL = {"https://www.netteller.com/releasetest9325","https://www2.netteller.com/releasetest9325","https://www.netteller.com/techbank8897","https://www2.netteller.com/sym6404" };
+
+            // Create web client and condition it
+            WebClient client = new WebClient();
+            client.Proxy = null;
+
+            // Download string.
+            this.Text = "Hamster Dance - Running!";
+
+            // ok tell the master hamster the time it started
+            DateTime currentTime = DateTime.Now;
+            textBox3.Text = currentTime.ToString();
+            textBox3.Refresh();
+
+            Stopwatch sw = new Stopwatch();
+
+            foreach (string URLtest in textBox2.Lines)
+            {
+                try
+                {
+
+                    sw.Start(); // timing this activity
+                    string value = client.DownloadString(@URLtest);
+                    sw.Stop();
+
+                    // Write values.
+                    textBox1.AppendText(URLtest + "|" + value.Length + "|" + client.BaseAddress.ToString() + "Time elasped: " + sw.Elapsed.TotalSeconds + "\n");
+                    sw.Reset();
+                }
+                catch
+                {
+                    textBox1.AppendText(URLtest + "|" + "NO Workie" + "\n");
+                }
+            }
+            this.Text = "Hamster Dance";
+
+            // close time
+            currentTime = DateTime.Now;
+            textBox4.Text = currentTime.ToString();
+            textBox4.Refresh();
+        }
+
+        public string[] toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dr = new OpenFileDialog();
+            DialogResult drOut = dr.ShowDialog();
+
+            if (drOut == DialogResult.Cancel)
+            {
+                dr.Dispose();
+                return null;
+            }
+            else if (drOut == DialogResult.OK)
+            {
+                string pathIN = dr.FileName;
+                string[] URLout = System.IO.File.ReadAllLines(@pathIN);
+                dr.Dispose();
+                return URLout;
+            }
+            return null;
+        }
+
+        private void toolStripMenuItem1_Click()
+        {
+
+        }
+
+        public string [] loadListOfURLsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = openFileDialog1.ShowDialog();
+
+            if (dr == DialogResult.Cancel)
+            {
+                openFileDialog1.Dispose();
+                return null;
+            }
+            else if (dr == DialogResult.OK)
+            {
+                string pathIN = openFileDialog1.FileName;
+                string[] URLout = System.IO.File.ReadAllLines(@pathIN);
+                openFileDialog1.Dispose();
+                return URLout;
+            }
+            return null;
+        }
+
+        private void loadURLsToTestToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = openFileDialog1.ShowDialog();
+
+            if (dr == DialogResult.Cancel)
+            {
+                openFileDialog1.Dispose();
+            }
+            else if (dr == DialogResult.OK)
+            {
+                string pathIN = openFileDialog1.FileName;
+                string[] URLout = System.IO.File.ReadAllLines(@pathIN);
+
+                 // load textBox2
+                foreach (string urlLine in URLout)
+                {
+                    textBox2.AppendText(urlLine + "\n");
+                }
+
+                openFileDialog1.Dispose();
+            }
+        }
+
+        private void copyOutTheResultsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfOut = new SaveFileDialog();
+            sfOut.FileName = "mystuffOut.csv";
+
+            DialogResult putHereBaby = sfOut.ShowDialog();
+            string filename = sfOut.FileName;
+
+            try
+            {
+                //load textbox1 to the file selected
+                File.WriteAllLines(filename, textBox1.Lines);
+            }
+            catch
+            {
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+   }
+}
